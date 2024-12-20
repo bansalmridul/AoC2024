9d0201dd0eb5162ceca20ac02a49173cfff54173
Mridul Bansal <bansal.mridul@gmail.com>
2024-12-20 09:27:22 -0700

    i don't understand git. Also improved day 20

--- 20.py
+++ 20.py
@@ -1,7 +1,6 @@
-import os
+import os, time
 import numpy as np
 import itertools
-import time
 
 dir_list = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
@@ -22,30 +21,7 @@ def dfs(grid, s_val):
     return 1
 
 
-def saves_threshold(grid, t):
-    ret = 0
-    for x, y in np.ndindex(grid.shape):
-        for dir in dir_list:
-            ht = grid[x][y]  # honest time
-            if ht < t:  # not enough time to save
-                continue
-            p1x, p1y = x + dir[0], y + dir[1]
-            if (not (0 < p1x < len(grid) - 1)) or (not (0 < p1y < len(grid[0]) - 1)) or grid[p1x][p1y] >= 0:  # border coord or not a barrier
-                continue
-            p2x, p2y = x + 2 * dir[0], y + 2 * dir[1]
-            # t <= ts = ht - grid(p2) - 2
-            if (
-                (not (0 < p2x < len(grid) - 1)) or (not (0 < p2y < len(grid[0]) - 1)) or -1 == grid[p2x][p2y] or ht - grid[p2x][p2y] - 2 < t
-            ):  # border coord or barrier or insufficient time save
-                continue
-            ret += 1
-    return ret
-
-
-# alternative solution is to grid a map of dist to point, and iterate through all of the points that are at least
-# threshold away. Then check if in range and if it saves t time
-# would implement this, but I'm tired and my solution works quickly enough
-def saves_threshold_extra(grid, t):
+def saves_threshold(grid, t, sc):
     ret = 0
     index = np.where(grid == 0)
     ex, ey = index[0][0], index[1][0]
@@ -53,8 +29,8 @@ def saves_threshold_extra(grid, t):
         ht = grid[x][y]  # honest time
         if ht < t + abs(x - ex) + abs(y - ey):
             continue  # ht must be at least dist more than threshold to save time
-        for dx in range(-20, 21, 1):
-            for dy in range(-1 * (20 - abs(dx)), (21 - abs(dx)), 1):  # iterate through all points up to dist20 away
+        for dx in range(-1 * sc, sc + 1, 1):
+            for dy in range(-1 * (sc - abs(dx)), (sc + 1 - abs(dx)), 1):  # iterate through all points up to dist<=sc away
                 cx, cy = x + dx, y + dy  # cheat x, y
                 if cx < 0 or cx >= len(grid) or cy < 0 or cy >= len(grid[0]) or grid[cx][cy] == -1:
                     continue  # not in grid or barrier
@@ -82,8 +58,8 @@ def part1and2(test=False):
         row = np.array([convert.get(c, def_val) for c in line.strip()]).reshape(1, -1)
         grid = np.vstack((grid, row))
     dfs(grid, s_val)
-    print(saves_threshold(grid, t))
-    print(saves_threshold_extra(grid, t2))
+    print(saves_threshold(grid, t, 2))
+    print(saves_threshold(grid, t2, 20))
 
 
 def main():
