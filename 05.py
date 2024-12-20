113a043befb495f1c9b77cbb9d38d46c5ed5c406
Mridul Bansal <bansal.mridul@gmail.com>
2024-12-19 22:08:47 -0700

    cleanup for day 5

--- 05.py
+++ 05.py
@@ -1,58 +1,38 @@
-import copy
-import math
-import random
 from collections import defaultdict
+import os
 
-# Python program to print topological sorting of a DAG
-from collections import defaultdict
-
-
-# Class to represent a graph
 class Graph:
     def __init__(self, vertices):
         self.graph = defaultdict(list)  # dictionary containing adjacency List
         self.V = vertices  # set of vertices
 
-    # function to add an edge to graph
     def addEdge(self, u, v):
         self.graph[u].append(v)
 
-    # A recursive function used by topologicalSort
     def topologicalSortUtil(self, v, visited, stack):
 
-        # Mark the current node as visited.
         visited[v] = True
-        # Recur for all the vertices adjacent to this vertex
         for i in self.graph[v]:
             if i in visited and visited[i] == False:
                 self.topologicalSortUtil(i, visited, stack)
 
-        # Push current vertex to stack which stores result
         stack.insert(0, v)
-        # print(f"inserted: {v}")
 
-    # The function to do Topological Sort. It uses recursive
-    # topologicalSortUtil()
     def topologicalSort(self):
-        # Mark all the vertices as not visited
         visited = {key: False for key in self.V}
         stack = []
-
-        # Call the recursive helper function to store Topological
-        # Sort starting from all vertices one by one
         for i in list(self.V):
             if visited[i] == False:
                 self.topologicalSortUtil(i, visited, stack)
-
-        # Print contents of stack
         return stack
 
 
 def part1(test=False):
+    fn = os.path.basename(__file__)
     if test:
-        fn = f"input{__file__[:-3]}-t.txt"
+        fn = f"input{fn[:-3]}-t.txt"
     else:
-        fn = f"input{__file__[:-3]}.txt"
+        fn = f"input{fn[:-3]}.txt"
     file1 = open(fn, "r+")
     arrt = file1.readlines()
     ret = 0
@@ -80,10 +60,11 @@ def part1(test=False):
 
 
 def part2(test=False):
+    fn = os.path.basename(__file__)
     if test:
-        fn = f"input{__file__[:-3]}-t.txt"
+        fn = f"input{fn[:-3]}-t.txt"
     else:
-        fn = f"input{__file__[:-3]}.txt"
+        fn = f"input{fn[:-3]}.txt"
     file1 = open(fn, "r+")
     arrt = file1.readlines()
     ret = 0
